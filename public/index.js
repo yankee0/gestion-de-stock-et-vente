6fa96f1268b75324578154dfa67c7ea258b9b5e8
yankee <yankeesuprem@gmail.com>
2024-05-09 23:14:51 +0000

    adding button

--- public/index.js
+++ public/index.js
@@ -5,19 +5,37 @@ const MainContent = () => {
         <div className="card-title">Formulaire de création</div>
         <p>Ajouter un produit à la facture</p>
         <div className="row">
-            <div className="col-md-4 col-lg-4 col-xl-3">
-              <div className="mb-3">
-                <label for="profile" className="form-label">
-                  Sélectionnez le produit
-                </label>
-                <select className="form-select " name="profile" id="profile">
-                </select>
-              </div>
+          <div className="col-md-4 col-lg-4 col-xl-3">
+            <div className="mb-3">
+              <label for="profile" className="form-label">
+                Sélectionnez le produit
+              </label>
+              <select
+                className="form-select "
+                name="profile"
+                id="profile"
+              ></select>
             </div>
-            <div class="col-md-4 col-lg-4 col-xl-3">
+          </div>
+          <div class="col-md-4 col-lg-4 col-xl-3">
             <div class="mb-3">
-              <label for="quantity" class="form-label">Quantité (en Kg ou en Unité)</label>
-              <input value="" required type="number" class="form-control" name="quantity" id="quantity" min="0" />
+              <label for="quantity" class="form-label">
+                Quantité (en Kg ou en Unité)
+              </label>
+              <input
+                value=""
+                required
+                type="number"
+                class="form-control"
+                name="quantity"
+                id="quantity"
+                min="0"
+              />
+            </div>
+          </div>
+          <div class="col-md-4 col-lg-4 col-xl-3 d-flex">
+            <div className="flex-fill d-flex align-items-end">
+              <button className="mb-3 btn btn-primary">Ajouter</button>
             </div>
           </div>
         </div>
